fb5386c1998f773b9f266562c37d4a21a4dfc68e
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-02-18 13:46:53 -0500

    Finish testing for Model Manager, fix construct_point to use Date object

--- testing/test_thruhike.rb
+++ testing/test_thruhike.rb
@@ -26,31 +26,31 @@ class ThruHikeTest < MiniTest::Test
 
     @user1 = User.new("User One", "user_one_1").save
     @incomplete_hike_zero_start = Hike.new(@user1, 0.0, 2194.3, "Incomplete Hike Zero Start", false).save
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 10), 8.1).save
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 11), 15.7).save
+    @incomplete_hike_zero_start.create_new_point(8.1, Date.new(2022, 4, 10)).save
+    @incomplete_hike_zero_start.create_new_point(15.7, Date.new(2022, 4, 11)).save
 
     @user2 = User.new("User Two", "user_two_2").save
     @complete_hike_non_zero_start = Hike.new(@user2, 50.0, 150.0, "Complete Hike Non-zero Start", false).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2021, 12, 29), 58.3).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2021, 12, 30), 71.3).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2021, 12, 31), 80.85).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 1), 89.85).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 2), 104.6).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 3), 124.6).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 4), 124.6).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 5), 135.2).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 6), 135.2).save
-    @complete_hike_non_zero_start.create_new_point(Date.new(2022, 1, 7), 150.0).save
+    @complete_hike_non_zero_start.create_new_point(58.3, Date.new(2021, 12, 29)).save
+    @complete_hike_non_zero_start.create_new_point(71.3, Date.new(2021, 12, 30)).save
+    @complete_hike_non_zero_start.create_new_point(80.85, Date.new(2021, 12, 31)).save
+    @complete_hike_non_zero_start.create_new_point(89.85, Date.new(2022, 1, 1)).save
+    @complete_hike_non_zero_start.create_new_point(104.6, Date.new(2022, 1, 2)).save
+    @complete_hike_non_zero_start.create_new_point(124.6, Date.new(2022, 1, 3)).save
+    @complete_hike_non_zero_start.create_new_point(124.6, Date.new(2022, 1, 4)).save
+    @complete_hike_non_zero_start.create_new_point(135.2, Date.new(2022, 1, 5)).save
+    @complete_hike_non_zero_start.create_new_point(135.2, Date.new(2022, 1, 6)).save
+    @complete_hike_non_zero_start.create_new_point(150.0, Date.new(2022, 1, 7)).save
     @complete_hike_non_zero_start.mark_complete
 
     @second_hike_incomplete = Hike.new(@user2, 0.0, 30.0, "Short Hike Incomplete", false).save
-    @second_hike_incomplete.create_new_point(Date.new(2023, 1, 12), 4.2)
-    @second_hike_incomplete.create_new_point(Date.new(2023, 1, 13), 9.3)
+    @second_hike_incomplete.create_new_point(4.2, Date.new(2023, 1, 12))
+    @second_hike_incomplete.create_new_point(9.3, Date.new(2023, 1, 13))
   end
 
   def test_adding_point_to_unsaved_hike
     @unsaved_hike = Hike.new(@user1, 0, 100, "test hike", false)
-    point = @unsaved_hike.create_new_point(Date.new, 11.1)
+    point = @unsaved_hike.create_new_point(11.1, Date.new)
     assert_raises(NoMatchingPKError) { point.save }
   end
 
@@ -63,18 +63,18 @@ class ThruHikeTest < MiniTest::Test
   def test_average_mileage_per_day
     assert_equal(7.85, @incomplete_hike_zero_start.average_mileage_per_day)
 
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 12), 26.3).save
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 13), 32.4).save
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 14), 42.8).save
+    @incomplete_hike_zero_start.create_new_point(26.3, Date.new(2022, 4, 12)).save
+    @incomplete_hike_zero_start.create_new_point(32.4, Date.new(2022, 4, 13)).save
+    @incomplete_hike_zero_start.create_new_point(42.8, Date.new(2022, 4, 14)).save
     assert_equal(8.56, @incomplete_hike_zero_start.average_mileage_per_day)
   end
 
   def test_mileage_to_finish
     assert_equal(2178.6, @incomplete_hike_zero_start.mileage_from_finish)
 
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 12), 26.3).save
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 13), 32.4).save
-    @incomplete_hike_zero_start.create_new_point(Date.new(2022, 4, 14), 42.8).save
+    @incomplete_hike_zero_start.create_new_point(26.3, Date.new(2022, 4, 12)).save
+    @incomplete_hike_zero_start.create_new_point(32.4, Date.new(2022, 4, 13)).save
+    @incomplete_hike_zero_start.create_new_point(42.8, Date.new(2022, 4, 14)).save
 
     assert_equal(2151.5, @incomplete_hike_zero_start.mileage_from_finish)
   end
@@ -99,5 +99,21 @@ class ThruHikeTest < MiniTest::Test
     constructed_hikes = @manager.all_hikes_from_user(2)
     manual_hikes = [@complete_hike_non_zero_start, @second_hike_incomplete].sort
     assert_equal(manual_hikes, constructed_hikes)
+  end
+
+  def test_one_hike
+    assert_equal(@incomplete_hike_zero_start, @manager.one_hike(1))
+    assert_equal(@second_hike_incomplete, @manager.one_hike(3))
+    assert_equal(@complete_hike_non_zero_start, @manager.one_hike(2))
+  end
+
+  def test_all_points_from_hike
+    manual_points = [
+          Point.new(@incomplete_hike_zero_start, 8.1, Date.new(2022, 4, 10)),
+          Point.new(@incomplete_hike_zero_start, 15.7, Date.new(2022, 4, 11))
+    ].sort
+    constructed_points = @manager.all_points_from_hike(1)
+
+    assert_equal(manual_points, constructed_points)
   end
 end
