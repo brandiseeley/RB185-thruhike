941897f8df2438c0ea56a899b081fbf8693cfd1f
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-02-27 10:21:32 -0500

    Ability to add new point

--- thruhike_app.rb
+++ thruhike_app.rb
@@ -10,7 +10,7 @@ require_relative "models"
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
-  also_reload "database_persistence.rb", "model_manager.rb", "thruhike.rb"
+  also_reload "database_persistence.rb", "model_manager.rb", "thruhike.rb" if development?
 end
 
 def logged_in_user
@@ -26,6 +26,16 @@ end
 def valid_hike?(name, start_mileage, finish_mileage, user)
   # Validation goes here
   true
+end
+
+def valid_point?(hike, mileage, date)
+  # Validation goes here
+  true
+end
+
+def require_login
+  session[:message] = "You must be logged in to do that"
+  redirect "/"
 end
 
 before do
@@ -46,7 +56,7 @@ post "/hikes" do
 end
 
 get "/hikes" do
-  redirect "/" unless logged_in?
+  require_login unless logged_in?
   @user = logged_in_user
   # TODO : Handle bad status
   @hikes = @manager.all_hikes_from_user(@user.id).data
@@ -54,13 +64,14 @@ get "/hikes" do
 end
 
 get "/hikes/new" do
-  redirect "/" unless logged_in?
+  require_login unless logged_in?
   @user = logged_in_user
 
   erb :new_hike
 end
 
 post "/hikes/new" do
+  require_login unless logged_in?
   hike_name = params[:name]
   start_mileage = params[:start_mileage].to_f
   finish_mileage = params[:finish_mileage].to_f
@@ -83,7 +94,7 @@ post "/hikes/new" do
 end
 
 get "/hikes/:hike_id" do
-  redirect "/" unless logged_in?
+  require_login unless logged_in?
   hike_id = params["hike_id"].to_i
   @user = logged_in_user
   # TODO : Handle bad status
@@ -91,4 +102,23 @@ get "/hikes/:hike_id" do
   @points = @manager.all_points_from_hike(hike_id).data
   @stats = @manager.hike_stats(@hike)
   erb :hike
+end
+
+post "/hikes/:hike_id" do
+  require_login unless logged_in?
+  hike_id = params["hike_id"].to_i
+  date = params[:date]
+  mileage = params[:mileage]
+  hike = @manager.one_hike(hike_id).data
+
+  if valid_point?(hike, mileage, date)
+    point = Point.new(hike, mileage, date)
+    status = @manager.insert_new_point(point)
+    if status.success
+      session[:message] = "Point successfully created"
+      redirect "/hikes/#{hike_id}"
+    end
+  end
+  session[:message] = "There was an error, point creation unsuccessful"
+  redirect "/hikes/#{hike_id}"
 end
