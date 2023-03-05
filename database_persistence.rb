56653e4f4fb83000712e36112e9bb120ed6cb566
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-03-05 16:13:00 -0600

    Fix zero division error for average mileage per day

--- database_persistence.rb
+++ database_persistence.rb
@@ -92,6 +92,14 @@ class DatabasePersistence
   # This is throwing a zero division error When there is a point with mileage
   # equal to the start mileage, but only when that is the only point
   def average_mileage_per_day(hike)
+    points_attempt = all_points_from_hike(hike.id)
+    return points_attempt unless points_attempt.success
+
+    return query("SELECT 0.0") if points_attempt.data.ntuples.zero?
+
+    mileages = points_attempt.data.field_values("mileage")
+    return query("SELECT 0.0") if mileages.all? { |mileage| mileage == hike.start_mileage }
+
     sql = <<-SQL
           select round( ( max(mileage) - max(start_mileage) ) / 
           CASE
